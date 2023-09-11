6ce9ab02d2d0fefba262460e152a68c574b6d4e9
Vladimir Mikhalev <ask@sre.gg>
2023-09-11 18:31:06 -0400

    Grafana with Let's Encrypt Using Docker Compose

---
+++ grafana-restore-application-data.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# # grafana-restore-application-data.sh Description
+# This script is designed to restore the application data.
+# 1. **Identify Containers**: Similarly to the database restore script, it identifies the service and backups containers by name.
+# 2. **List Application Data Backups**: Displays all available application data backups at the specified backup path.
+# 3. **Select Backup**: Asks the user to copy and paste the desired backup name for application data restoration.
+# 4. **Stop Service**: Stops the service to prevent any conflicts during the restore process.
+# 5. **Restore Application Data**: Removes the current application data and then extracts the selected backup to the appropriate application data path.
+# 6. **Start Service**: Restarts the service after the application data has been successfully restored.
+# To make the `grafana-restore-application-data.sh` script executable, run the following command:
+# `chmod +x grafana-restore-application-data.sh`
+# By utilizing this script, you can efficiently restore application data from an existing backup while ensuring proper coordination with the running service.
+
+GRAFANA_CONTAINER=$(docker ps -aqf "name=grafana-grafana")
+GRAFANA_BACKUPS_CONTAINER=$(docker ps -aqf "name=grafana-backups")
+BACKUP_PATH="/srv/grafana-application-data/backups/"
+RESTORE_PATH="/opt/bitnami/grafana/data/"
+BACKUP_PREFIX="grafana-application-data"
+
+echo "--> All available application data backups:"
+
+for entry in $(docker container exec -it "$GRAFANA_BACKUPS_CONTAINER" sh -c "ls $BACKUP_PATH")
+do
+  echo "$entry"
+done
+
+echo "--> Copy and paste the backup name from the list above to restore application data and press [ENTER]
+--> Example: ${BACKUP_PREFIX}-backup-YYYY-MM-DD_hh-mm.tar.gz"
+echo -n "--> "
+
+read SELECTED_APPLICATION_BACKUP
+
+echo "--> $SELECTED_APPLICATION_BACKUP was selected"
+
+echo "--> Stopping service..."
+docker stop "$GRAFANA_CONTAINER"
+
+echo "--> Restoring application data..."
+docker exec -it "$GRAFANA_BACKUPS_CONTAINER" sh -c "rm -rf ${RESTORE_PATH}* && tar -zxpf ${BACKUP_PATH}${SELECTED_APPLICATION_BACKUP} -C /"
+echo "--> Application data recovery completed..."
+
+echo "--> Starting service..."
+docker start "$GRAFANA_CONTAINER"
--- grafana-restore-database.sh
+++ grafana-restore-database.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-GRAFANA_CONTAINER=$(docker ps -aqf "name=grafana_grafana")
-GRAFANA_BACKUPS_CONTAINER=$(docker ps -aqf "name=grafana_backups")
+# # grafana-restore-database.sh Description
+# This script facilitates the restoration of a database backup.
+# 1. **Identify Containers**: It first identifies the service and backups containers by name, finding the appropriate container IDs.
+# 2. **List Backups**: Displays all available database backups located at the specified backup path.
+# 3. **Select Backup**: Prompts the user to copy and paste the desired backup name from the list to restore the database.
+# 4. **Stop Service**: Temporarily stops the service to ensure data consistency during restoration.
+# 5. **Restore Database**: Executes a sequence of commands to drop the current database, create a new one, and restore it from the selected compressed backup file.
+# 6. **Start Service**: Restarts the service after the restoration is completed.
+# To make the `grafana-restore-database.shh` script executable, run the following command:
+# `chmod +x grafana-restore-database.sh`
+# Usage of this script ensures a controlled and guided process to restore the database from an existing backup.
+
+GRAFANA_CONTAINER=$(docker ps -aqf "name=grafana-grafana")
+GRAFANA_BACKUPS_CONTAINER=$(docker ps -aqf "name=grafana-backups")
+GRAFANA_DB_NAME="grafanadb"
+GRAFANA_DB_USER="grafanadbuser"
+POSTGRES_PASSWORD=$(docker exec $GRAFANA_BACKUPS_CONTAINER printenv PGPASSWORD)
+BACKUP_PATH="/srv/grafana-postgres/backups/"
 
 echo "--> All available database backups:"
 
-for entry in $(docker container exec -it $GRAFANA_BACKUPS_CONTAINER sh -c "ls /srv/grafana-postgres/backups/")
+for entry in $(docker container exec "$GRAFANA_BACKUPS_CONTAINER" sh -c "ls $BACKUP_PATH")
 do
   echo "$entry"
 done
@@ -19,13 +35,13 @@ read SELECTED_DATABASE_BACKUP
 echo "--> $SELECTED_DATABASE_BACKUP was selected"
 
 echo "--> Stopping service..."
-docker stop $GRAFANA_CONTAINER
+docker stop "$GRAFANA_CONTAINER"
 
 echo "--> Restoring database..."
-docker exec -it $GRAFANA_BACKUPS_CONTAINER sh -c 'PGPASSWORD="$(echo $POSTGRES_PASSWORD)" dropdb -h postgres -p 5432 grafanadb -U grafanadbuser \
-&& PGPASSWORD="$(echo $POSTGRES_PASSWORD)" createdb -h postgres -p 5432 grafanadb -U grafanadbuser \
-&& PGPASSWORD="$(echo $POSTGRES_PASSWORD)" gunzip -c /srv/grafana-postgres/backups/'$SELECTED_DATABASE_BACKUP' | PGPASSWORD=$(echo $POSTGRES_PASSWORD) psql -h postgres -p 5432 grafanadb -U grafanadbuser'
+docker exec "$GRAFANA_BACKUPS_CONTAINER" sh -c "dropdb -h postgres -p 5432 $GRAFANA_DB_NAME -U $GRAFANA_DB_USER \
+&& createdb -h postgres -p 5432 $GRAFANA_DB_NAME -U $GRAFANA_DB_USER \
+&& gunzip -c ${BACKUP_PATH}${SELECTED_DATABASE_BACKUP} | psql -h postgres -p 5432 $GRAFANA_DB_NAME -U $GRAFANA_DB_USER"
 echo "--> Database recovery completed..."
 
 echo "--> Starting service..."
-docker start $GRAFANA_CONTAINER
+docker start "$GRAFANA_CONTAINER"
